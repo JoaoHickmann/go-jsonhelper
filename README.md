eacc4c13148ef0210d0218167a50910ce0e783af
João Pedro Hickmann <joaophickmann@outlook.com>
2019-01-17 01:41:18 -0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,37 @@
+# go-jsonhelper #
+
+```go
+package main
+
+import (
+	"fmt"
+
+	"github.com/JoaoHickmann/go-jsonhelper/jsonhelper"
+)
+
+func main() {
+	json := `
+		{
+			"string": "string",
+			"number": 2.5,
+			"bool": true,
+			"array": [
+				1, 
+				2,
+				3
+			],
+			"object": {
+				"key": "value"
+			}
+		}
+	`
+
+	jsonHelper := jsonhelper.NewJSONHelper([]byte(json))
+
+	fmt.Println(jsonHelper.Map()["string"].String())
+	fmt.Println(jsonHelper.Map()["number"].Number())
+	fmt.Println(jsonHelper.Map()["bool"].Bool())
+	fmt.Println(jsonHelper.Map()["array"].Array()[0].Number())
+	fmt.Println(jsonHelper.Map()["object"].Map()["key"].String())
+}
+```
---
+++ jsonhelper/jsonhelper.go
@@ -0,0 +1,65 @@
+package jsonhelper
+
+import (
+	"encoding/json"
+)
+
+type jsonField struct {
+	data interface{}
+}
+
+type JSONHelper interface {
+	Data() interface{}
+
+	Bool() bool
+	String() string
+	Number() float64
+	Map() map[string]JSONHelper
+	Array() []JSONHelper
+}
+
+func NewJSONHelper(data []byte) JSONHelper {
+	var jsonResponse map[string]interface{}
+	err := json.Unmarshal(data, &jsonResponse)
+	if err != nil {
+		var jsonResponse2 []interface{}
+		err := json.Unmarshal(data, &jsonResponse2)
+		if err != nil {
+			panic("Invalid JSON!")
+		}
+		return jsonField{jsonResponse2}
+	}
+	return jsonField{jsonResponse}
+}
+
+func (jsonfield jsonField) Data() interface{} {
+	return jsonfield.data
+}
+
+func (jsonfield jsonField) Bool() bool {
+	return jsonfield.data.(bool)
+}
+
+func (jsonfield jsonField) String() string {
+	return jsonfield.data.(string)
+}
+
+func (jsonfield jsonField) Number() float64 {
+	return jsonfield.data.(float64)
+}
+
+func (jsonfield jsonField) Array() []JSONHelper {
+	result := make([]JSONHelper, 0)
+	for _, value := range jsonfield.data.([]interface{}) {
+		result = append(result, jsonField{value})
+	}
+	return result
+}
+
+func (jsonfield jsonField) Map() map[string]JSONHelper {
+	result := make(map[string]JSONHelper)
+	for key, value := range jsonfield.data.(map[string]interface{}) {
+		result[key] = jsonField{value}
+	}
+	return result
+}
