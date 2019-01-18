20b443c02480fb0271cb7c775ea9f2068dd95472
João Pedro Hickmann <joaophickmann@outlook.com>
2019-01-18 14:41:27 -0200

    code cleaning

--- jsonhelper/jsonhelper.go
+++ jsonhelper/jsonhelper.go
@@ -19,22 +19,27 @@ type JSONHelper interface {
 	Array() []JSONHelper
 }
 
-func NewJSONHelper(data []byte) (JSONHelper, error) {
+var ErrInvalidJSON = errors.New("jsonhelper: invalid JSON")
+
+func NewJSONHelper(data []byte) (jsonHelper JSONHelper, err error) {
 	var jsonResponse map[string]interface{}
-	err := json.Unmarshal(data, &jsonResponse)
+	err = json.Unmarshal(data, &jsonResponse)
 	if err != nil {
 		var jsonResponse2 []interface{}
-		err := json.Unmarshal(data, &jsonResponse2)
+		err = json.Unmarshal(data, &jsonResponse2)
 		if err != nil {
-			return nil, errors.New("invalid JSON")
+			err = ErrInvalidJSON
+			return
 		}
-		return jsonField{jsonResponse2}, nil
+		jsonHelper = jsonField{jsonResponse2}
 	}
-	return jsonField{jsonResponse}, nil
+	jsonHelper = jsonField{jsonResponse}
+	return
 }
 
-func (jsonfield jsonField) Data() interface{} {
-	return jsonfield.data
+func (jsonfield jsonField) Data() (result interface{}) {
+	result = jsonfield.data
+	return
 }
 
 func (jsonfield jsonField) Bool() (result bool) {
@@ -54,20 +59,18 @@ func (jsonfield jsonField) Number() (result float64) {
 
 func (jsonfield jsonField) Array() (result []JSONHelper) {
 	result = []JSONHelper{}
-
 	data, _ := jsonfield.data.([]interface{})
 	for _, value := range data {
 		result = append(result, jsonField{value})
 	}
-	return result
+	return
 }
 
 func (jsonfield jsonField) Map() (result map[string]JSONHelper) {
 	result = map[string]JSONHelper{}
-
 	data, _ := jsonfield.data.(map[string]interface{})
 	for key, value := range data {
 		result[key] = jsonField{value}
 	}
-	return result
+	return
 }
